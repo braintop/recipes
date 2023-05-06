57194fb80246f4736901612ba7a64e8b9270ca45
braintop <braintop7@gmail.com>
2023-05-06 15:46:32 +0300

    start 2

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import firebase from './FirebaseConfig';
+function App() {
+  return (
+    <div className="App">
+      <div>helllo world</div>
+    </div>
+  );
+}
+
+export default App;
